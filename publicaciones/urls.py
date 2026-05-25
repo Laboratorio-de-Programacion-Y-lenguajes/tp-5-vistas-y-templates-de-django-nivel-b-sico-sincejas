33a452ac39a8b9810ba4ed8aabc1456a3f6b5869
pabloap280-cell <pabloap280@gmail.com>
2026-05-25 13:19:48 -0300

    feat(publicaciones): definir rutas para las vistas basadas en clases

--- publicaciones/urls.py
+++ publicaciones/urls.py
@@ -6,22 +6,11 @@ from . import views
 app_name = "publicaciones"
 
 urlpatterns = [
-    # TODO: Definir las tres rutas usando path() y .as_view()
-    #
-    # Rutas a implementar:
-    #
-    #   URL: ""
-    #   Vista: InicioView
-    #   Nombre: "inicio"
-    #
-    #   URL: "publicaciones/"
-    #   Vista: PublicacionListView
-    #   Nombre: "lista_publicaciones"
-    #
-    #   URL: "publicaciones/<int:publicacion_id>/"
-    #   Vista: PublicacionDetailView
-    #   Nombre: "detalle_publicacion"
-    #
-    # Pista para registrar una CBV:
-    #   path("ruta/", views.MiVista.as_view(), name="nombre"),
+    path("", views.InicioView.as_view(), name="inicio"),
+
+
+    path("publicaciones/", views.PublicacionListView.as_view(), name="lista_publicaciones"),
+
+
+    path("publicaciones/<int:publicacion_id>/", views.PublicacionDetailView.as_view(), name="detalle_publicacion") ,
 ]
